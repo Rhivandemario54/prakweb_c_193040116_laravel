d41d4b09eec2384d5971b8a106ce523c1a90a845
Rhivandemario54 <rhivandemario54@gmail.com>
2022-11-27 10:48:15 +0700

    update & delete post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -6,32 +6,31 @@
 
   @if (session()->has('success'))
     <div class="alert alert-success" role="alert">
+    <div class="alert alert-success col-lg-8" role="alert">
       {{ session('success') }}
     </div>
   @endif
-
-
-  <div class="table-responsive col-lg-8">
-    <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create New Post</a>
-    <table class="table table-striped table-sm">
-      <thead>
-        <tr>
+@@ -20,7 +20,7 @@
           <th scope="col">#</th>
           <th scope="col">Title</th>
           <th scope="col">Category</th>
           <th scope="col">Acction</th>
+          <th scope="col">Action</th>
         </tr>
       </thead>
       <tbody>
-        @foreach ($posts as $post)
-          <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $post->tittle }}</td>
+@@ -31,8 +31,12 @@
             <td>{{ $post->category->name }}</td>
             <td>
               <a href="/dashboard/posts/{{ $post->slug }}" class='badge bg-info' ><span data-feather='eye'></span></a>
               <a href="/dashboard/posts/{{ $post->slug }}" class='badge bg-warning' ><span data-feather='edit'></span></a>
               <a href="/dashboard/posts/{{ $post->slug }}" class='badge bg-danger' ><span data-feather='x-circle'></span></a>
+              <a href="/dashboard/posts/{{ $post->slug }}/edit" class='badge bg-warning' ><span data-feather='edit'></span></a>
+              <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                @method('delete') 
+                @csrf
+                <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather='x-circle'></span></button>
+              </form>
             </td>
           </tr>
         @endforeach
